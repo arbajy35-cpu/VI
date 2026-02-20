98ab77531c7ca440b86a7b8b112ed32f8df5112c
arbajy35-cpu <arbajy35@gmail.com>
2026-02-20 21:40:28 +0530

    VI-#3

--- app/src/main/java/com/nexora/vi/MainActivity.java
+++ app/src/main/java/com/nexora/vi/MainActivity.java
@@ -3,9 +3,7 @@ package com.nexora.vi;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,20 +12,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.main);
 
-        // Toolbar setup
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        // IMPORTANT: use correct ID (webView not webview)
+        // WebView setup
         webView = findViewById(R.id.webView);
-
         webView.getSettings().setJavaScriptEnabled(true);
-
         webView.setWebViewClient(new WebViewClient());
-
         webView.loadUrl("file:///android_asset/index.html");
     }
 
